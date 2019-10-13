71d7649e0636162f1faf18c08b212d685d79863d
2554509132@qq.com <2554509132@qq.com>
2019-10-13 23:12:33 +0800

    @babel 转码器

---
+++ tryBabel/app.js
@@ -0,0 +1,3 @@
+var arr = [1,2,23,3];
+
+arr.map(item => item + 1);
---
+++ tryBabel/tagDir/app.js
@@ -0,0 +1,7 @@
+"use strict";
+
+var arr = [1, 2, 23, 3];
+arr.map(function (item) {
+  return item + 1;
+});
+//# sourceMappingURL=app.js.map

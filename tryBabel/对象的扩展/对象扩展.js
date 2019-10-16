0f882b162454ec403ef9d10b03dd78ab064e2e46
2554509132@qq.com <2554509132@qq.com>
2019-10-16 17:07:48 +0800

    函数变量做对象属性 example

--- tryBabel/对象的扩展/对象扩展.js
+++ tryBabel/对象的扩展/对象扩展.js
@@ -16,11 +16,46 @@
 // console.log(f(1, 2));
 
 // example => 函数变量名做对象属性
-const obj = {
-	method(){
-		return 'Hello';
-	}
+// const obj = {
+// 	method(){
+// 		return 'Hello';
+// 	}
+// };
+// console.log(obj);// { method: [Function: method] }
+// console.log(obj.method());// Hello
+
+// another example
+let birth = '2000/01/01';
+const Person = {
+	name: '张三',
+	birth,// 调用全局环境中的 birth 变量
+	hello(){console.log('名字是', this.name)}
 };
-console.log(obj);// { method: [Function: method] }
-console.log(obj.method());// Hello
+console.log(Person);// { name: '张三', birth: '2000/01/01', hello: [Function: hello] }
+console.log(Person.birth);// 2000/01/01
+birth = '2000/01/02';
+console.log(Person.birth);// 2000/01/01
+Person.hello();// // 名字是 张三
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 

92e69414e7c1a90233f8907cad47ef9a75bef832
2554509132@qq.com <2554509132@qq.com>
2019-10-16 16:58:25 +0800

    参数变量做对象属性

--- tryBabel/对象的扩展/对象扩展.js
+++ tryBabel/对象的扩展/对象扩展.js
@@ -2,7 +2,19 @@
 属性的简洁表示法
 */
 // 大括号中直接写变量和函数
-const v = 'var';
-const f = function(){};
-const obj = {v, f};
-console.log(obj);
+
+// example
+// const v = 'var';
+// const f = function(){};
+// const obj = {v, f};
+// console.log(obj);// { v: 'var', f: [Function: f] }
+
+// example => 参数做对象属性
+// function f(x, y){
+// 	return {x, y};
+// };
+// console.log(f(1, 2));
+
+// example => 方法简写
+const obj = 
+

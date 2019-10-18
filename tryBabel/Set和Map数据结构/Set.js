c19d49f994f4ff605f05695bc95f8afc47cfa69f
2554509132@qq.com <2554509132@qq.com>
2019-10-18 13:18:08 +0800

    Set 的操作方法 add delete has clear

--- tryBabel/Set和Map数据结构/Set.js
+++ tryBabel/Set和Map数据结构/Set.js
@@ -40,10 +40,81 @@ Set 结构 值唯一 这个特性的应用
 // console.log(set);//  Set { NaN } 只有一个NaN，说明判断相等
 
 
+/*
+Set 实例的属性和方法
+实例方法分两大类：操作方法 遍历方法
+操作方法：add delete has clear
+遍历方法：Set.prototype.keys() Set.prototype.values() Set.prototype.entries() Set.prototype.forEach()
+*/
+
+// 操作方法 案例
+
+// example
+// let s = new Set();
+
+// s.add(1).add(2).add(2);
+// // size ==> 2
+// s.has(1);// ==> true
+// s.delte(2);// 删除 2
+
+/*
+Object 判断一个键 与 Set 判断
+*/
+// example
+// let properties = {
+// 	'w': 1,
+// 	'a': 2
+// };
+// if (properties[someName]) {}
+
+// const properties = new Set();
+// properties.add('a');
+// properties.add('b');
+// if (properties.has(someName)) {}
+
+// console.log(properties.w);
+// console.log(properties['w']);
+// console.log(properties.a);
+// console.log(properties['a']);
+// console.log(properties[a]);// 这是访问一个变量
+
+// Array.from() 可以将 Set 结构数据转为数组
+// const items = new Set([1, 2, 3, 4, 5]);
+// console.log(Array.from(items));
+
+// // 另一种数组去重的方法
+// function dedupe(array){
+// 	return Array.from(new Set(array));
+// };
+// console.log(dedupe([1,2,2,3]));// [1,2,3]
 
 
+// 遍历方法 案例
+/*
+按插入顺序遍历
+*/
+// example
+// keys values entries
+let set = new Set();
+set.add('green');
+set.add('blue');
+set.add('red');
 
+console.log(set);
 
+for(let item of set.keys()){
+	console.log(item);
+}
+for(let item of set.values()){
+	console.log(item);
+}
+for(let item of set.entries()){
+	console.log(item);
+}
+// for of 可以直接循环遍历 Set
+for(let item of set){
+	console.log(item);
+}
 
 
 

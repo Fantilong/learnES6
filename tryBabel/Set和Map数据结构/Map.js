195ad43046a996c2f913a777deddee84e884456f
2554509132@qq.com <2554509132@qq.com>
2019-10-23 16:41:58 +0800

    Map与object及JSON的相互转换

--- tryBabel/Set和Map数据结构/Map.js
+++ tryBabel/Set和Map数据结构/Map.js
@@ -18,21 +18,21 @@ ES6的Hash结构可以：值-值
 
 // 构造函数接收数组，使用了数组的 forEach方法结合 map.set() 遍历完成
 // Map 的 键 规则中，undefined 和 null 不同，NaN 相同
-let map = new Map();
+// let map = new Map();
 
-map.set(-0, 123);
-console.log(map.get(0));// same
-console.log(map.get(+0));// same
+// map.set(-0, 123);
+// console.log(map.get(0));// same
+// console.log(map.get(+0));// same
 
-map.set(true, 1);
-map.set('true', 2);
-console.log(map.get(true)); // difference
-console.log(map.get('true')); // difference
+// map.set(true, 1);
+// map.set('true', 2);
+// console.log(map.get(true)); // difference
+// console.log(map.get('true')); // difference
 
-map.set(undefined, 1);
-map.set(null, 2);
-console.log(map.get(undefined));// difference
-console.log(map.get(null));// difference
+// map.set(undefined, 1);
+// map.set(null, 2);
+// console.log(map.get(undefined));// difference
+// console.log(map.get(null));// difference
 
 
 /*
@@ -67,15 +67,90 @@ clear
 // Map 遍历 结合 map 和 filter
 // 先转成数组，在转成 Map
 // forEach 使用 this 绑定作用域
-const reporter = {
-	report: function(k, v){
-		console.log("Key: %s, Value: %s", k, v);
+// const reporter = {
+// 	report: function(k, v){
+// 		console.log("Key: %s, Value: %s", k, v);
+// 	}
+// };
+
+// map.forEach(function(v, k){
+// 	this.report(k, v);// 这个函数不能使用 => 因为 this是绑定了的
+// }, reporter);
+
+
+// // 定义 Map
+// const myMap = new Map()
+// .set(true, 7)
+// .set({foo: 3}, ['abc']);
+// // Map 转数组
+// console.log([...myMap]);
+
+// 数组转 Map
+// const m = new Map([[true, 7], [{foo: 3}, ['abc']]]);
+// console.log(typeof m.keys());
+
+// Map 转为对象
+function strMapToObj(strMap){
+	let obj = Object.create(null);
+	for (let [k, v] of strMap) {
+		obj[k] = v;
 	}
+	return obj;
 };
 
-map.forEach(function(v, k){
-	this.report(k, v);// 这个函数不能使用 => 因为 this是绑定了的
-}, reporter);
+// const myMap = new Map()
+// 	.set('yes', true)
+// 	.set('no', false);
+// console.log(strMapToObj(myMap));
+
+// // 对象转为 Map
+function objToStrMap(obj){
+	let strMap = new Map();
+	for (let k of Object.keys(obj)) {
+		strMap.set(k, obj[k]);
+	}
+	return strMap;
+};
+// console.log(objToStrMap({yes: true, no: false}));
+
+// Map 转 JSON
+// Map 键名都是字符串
+// function strMapToJson(strMap){
+// 	return JSON.stringify(strMapToObj(strMap));
+// };
+// console.log(strMapToJson(new Map().set('yes', true).set('no', false)));
+// // Map 键名有 非字符串
+// function mapToArrayJson(map){
+// 	return JSON.stringify([...map]);
+// };
+
+// let map = new Map().set(true, 7).set({foo: 3}, ['abc']);
+// console.log(mapToArrayJson(map));
+
+// JSON 转为 Map
+function jsonToStrMap(jsonStr){
+	return objToStrMap(JSON.parse(jsonStr));
+};
+console.log(jsonToStrMap('{"yes": true, "no": false}'));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 

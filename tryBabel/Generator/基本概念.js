02d14afc3ceb7b735cd26823ad6b786bed157fb5
2554509132@qq.com <2554509132@qq.com>
2019-10-25 00:43:15 +0800

    Generator yield 表达式

--- tryBabel/Generator/基本概念.js
+++ tryBabel/Generator/基本概念.js
@@ -7,18 +7,64 @@ Generator 函数是一个状态机，封装了多个内部状态
 分段执行 yield 表达式是暂停执行的标识
 */
 
-function* helloWorldGenerator(){
-	yield 'hello';
-	yield 'World';
-	return 'ending';
+// function* helloWorldGenerator(){
+// 	yield 'hello';
+// 	yield 'World';
+// 	return 'ending';
+// }
+
+// var hw = helloWorldGenerator();// 返回内部状态指针对象，即：遍历器对象
+
+// console.log(hw.next());         
+// console.log(hw.next());
+// console.log(hw.next());
+// console.log(hw.next());
+
+
+/*
+yield 表达式
+暂停标志
+记录函数暂停执行的位置
+可以返回任意多的 yield 
+调用 next() 才执行
+只能用在 Generator 函数里面
+*/
+
+function* f(){
+	console.log('执行了');
 }
 
-var hw = helloWorldGenerator();// 返回内部状态指针对象，即：遍历器对象
+var generator = f();
 
-console.log(hw.next());         
-console.log(hw.next());
-console.log(hw.next());
-console.log(hw.next());
+setTimeout(function(){
+	generator.next();
+}, 2000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 

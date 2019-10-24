8a303daa2fb4d9259ac209b20f6a2287e64c5be1
2554509132@qq.com <2554509132@qq.com>
2019-10-24 20:47:29 +0800

    Promise.prototype.catch 捕获 reject 和 Promise 实例内部运行异常

--- tryBabel/promise/promise.prototype方法.js
+++ tryBabel/promise/promise.prototype方法.js
@@ -45,8 +45,70 @@ then
 // 	err => console.log('rejected: ' + err), 
 // );
 
+/*
+catch 
+exception 总处理
+*/
+// example
+// getJSON('/posts.json').then(function(posts){
+// 	// ...
+// }).catch(function(error){
+// 	// 处理 getJSON 和 前一个回调函数运行时发生的错误
+// 	console.log('发生错误！', error);
+// })
 
+// example
+// p.then((value) => console.log('fulfilled:', value))
+	// .catch((err) => console.log('rejected:', err));
+// 上一轮函数运行的错误，回来下一轮中被处理
 
+// example
+// const promise = new Promise(function(resolve, reject){
+// 	throw new Error('test');
+// });
+// promise.catch(function(err){
+// 	console.log(err);
+// })
+
+/*
+建议 Promise 对象后面要跟 catch 方法，可以处理 Promise内部
+发生的错误
+catch 返回的还是一个 Promise 对象，可以继续调用实例方法
+*/
+// example
+// const someAsyncThing = function(){
+// 	return new Promise(function(resolve, reject){
+// 		// 下面报错
+// 		resolve(x + 2);
+// 	});
+// };
+// someAsyncThing()
+// .catch(function(err){
+// 	console.log('oh no', err);
+// })
+// .then(function(){
+// 	console.log('carry on')
+// });
+
+// // example catch 方法中，再抛出错误
+// const someAsyncThing = function(){
+// 	return new Promise(function(resolve, reject){
+// 		resolve(x + 2);
+// 	})
+// };
+
+// someAsyncThing().then(function(){
+// 	return someAsyncThing();
+// }).catch(function(err){
+// 	console.log('oh no', err);
+
+// 	// 报错
+// 	y + 2;
+// }).then(function(){
+// 	console.log('carry on');
+// })
+
+// catch 捕获 reject 和 Promise 内部运行异常
 
 
 

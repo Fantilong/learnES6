5afb727968a7e037d517e330e7f3290b2ff1daf3
2554509132@qq.com <2554509132@qq.com>
2019-10-27 22:47:19 +0800

    异步操作

---
+++ tryBabel/Generator/Generator函数的应用.js
@@ -0,0 +1,18 @@
+/*
+异步操作的同步化表达
+*/
+// example ==> 加载、隐藏UI及其数据
+// function* loadUI(){
+// 	showLoadingScreen();
+// 	yield loadUIDataAsynchronously();
+// 	hideLoadingScreen();
+// }
+
+// var loader = loadUI();
+
+// // 加载UI
+// loader.next();
+
+// // 卸载UI
+// loader.next();
+

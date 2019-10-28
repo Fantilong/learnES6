03affeb56c5956554cf61a885e3916696d0f2a53
2554509132@qq.com <2554509132@qq.com>
2019-10-28 16:54:20 +0800

    异步编程的 回调函数 实现；为什么回调函数的第一个参数要是 err，分段执行，两个上下文环境，无法捕捉不同环境中的Err,所以要在回调函数中接收异常

--- tryBabel/Generator/Generator函数的异步应用.js
+++ tryBabel/Generator/Generator函数的异步应用.js
@@ -1,30 +1,54 @@
 /*
-Generator 与 Async 
+传统的异步编程方法
+回调函数
+事件监听
+发布/订阅
+Promise对象
 */
 
-const fs = require('fs');
+/*
+基本概念
+异步：非连续完成，先执行一段，暂停，执行其他任务，暂停，回到原来暂停的位置，继续执行
+*/
 
-const readFile = function (fileName) {
-	return new Promise(function(resolve, reject){
-		fs.readFile(fileName, function(err, data){
-			if (err) return reject(err);
-			resolve(data);
-		})
-	});
-}
+/*
+回调函数 ==> JS 对异步编程的实现，把任务的第二段
+单独写到一个函数里面，等到重新执行的时候，就直接调用这个函数
+callback(重新调用)
+*/
+// example
+fs.readFile('url', 'utf-8', function(err, data){
+	if (err) throw err;
+	// Do something with data
+});
+// ## 任务分段执行，上一段的上下文结束后，无法捕捉错误，智能当做参数
+// 传入第二段。
+/*
+为什么 Node 约定，回调函数的第一个参数，
+必须是错误对象err（如果没有错误，该参数就是null）？
+原因是执行分成两段，第一段执行完以后，任务所在的上下文
+环境就已经结束了。在这以后抛出的错误，原来的上下文环境
+已经无法捕捉，只能当作参数，传入第二段。
+*/
 
-const gen = function* (){
-	const f1 = yield readFile('/etc/fstsb');
-	const f2 = yield readFile('/etc/shells');
-	console.log(f1.toString());
-	console.log(f2.toString());
-};
 
-// gen 函数改写
-const asyncReadFile = async function(){
-	const f1 = await readFile('/etc/fstsb');
-	const f2 = await readFile('/etc/shells');
-	console.log(f1.toString());
-	console.log(f2.toString());
-};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 

e6241c8c9b3503c4a7190898e70eb50e7079f8c5
2554509132@qq.com <2554509132@qq.com>
2019-10-28 14:56:46 +0800

    使用 Generator 函数同步化表达异步操作

--- tryBabel/Generator/Generator函数的应用.js
+++ tryBabel/Generator/Generator函数的应用.js
@@ -16,3 +16,57 @@
 // // 卸载UI
 // loader.next();
 
+/*
+Generator 函数部署 Ajax 操作，同步表达
+*/
+// example
+// function* main(){
+// 	var result = yield request("http://some.url");
+// 	var resp = JSON.parse(result);
+// 	console.log(resp.value);
+// };
+
+// function request(url){
+// 	makeAjaxCall(url, function(response){
+// 		it.next(response);
+// 	});
+// };
+// var it = main();
+// it.next();
+
+/*
+Generator 函数逐行读取文本文件
+*/
+// example
+function* numbers(){
+	let file = new FileReader('numbers.txt');
+	try {
+		while(!file.eof){
+			yield parseInt(file.readLine(), 10);
+		}
+	}
+	finally {
+		file.close();
+	}
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

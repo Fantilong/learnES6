99a9f43ad3adf972d8d12d9603f87857233cce29
2554509132@qq.com <2554509132@qq.com>
2019-10-28 15:23:43 +0800

    Generator 函数控制执行流

--- tryBabel/Generator/Generator函数的应用.js
+++ tryBabel/Generator/Generator函数的应用.js
@@ -38,17 +38,91 @@ Generator 函数部署 Ajax 操作，同步表达
 Generator 函数逐行读取文本文件
 */
 // example
-function* numbers(){
-	let file = new FileReader('numbers.txt');
-	try {
-		while(!file.eof){
-			yield parseInt(file.readLine(), 10);
-		}
-	}
-	finally {
-		file.close();
-	}
-};
+// function* numbers(){
+// 	let file = new FileReader('numbers.txt');
+// 	try {
+// 		while(!file.eof){
+// 			yield parseInt(file.readLine(), 10);
+// 		}
+// 	}
+// 	finally {
+// 		file.close();
+// 	}
+// };
+
+/*
+控制流管理
+*/
+// 采用回调函数的方式 ==> 回调地狱
+// step1(function (value1){
+// 	step2(function (value2){
+// 		step3(function (value3){
+// 			step4(function (value4){
+// 				// Do something with value4
+// 			});
+// 		});
+// 	});
+// });
+
+// // Promise 写法
+// Promise.resolve(step1)
+// 	.then(step2)
+// 	.then(step3)
+// 	.then(step4)
+// 	.then(function (value4){
+// 		// Do something with value4
+// 	}, function (err){
+// 		// Handle any error from step1 through step4
+// 	})
+// 	.done();
+
+// Generator 写法
+// function* longRunningTask(value1){
+// 	try {
+// 		var value2 = yield step1(value1);
+// 		var value3 = yield step1(value2);
+// 		var value4 = yield step1(value3);
+// 		var value5 = yield step1(value4);
+// 		// Do something with value4
+// 	}
+// 	catch(e){
+// 		// Handle any error from step1 through step4
+// 	}
+// }
+// // 使用一个函数，按次序自动执行所有步骤
+// function scheduler(task){
+// 	var taskObj = task.next(task.value);
+// 	// 如果 Generator 函数未结束，就继续调用
+// 	if (!taskObj.done) {
+// 		task.value = taskObj.value;
+// 		scheduler(task);
+// 	}
+// }
+// 不能有异步操作
+
+// example ==> for 循环执行 yield 命令特性，控制流管理
+// let steps = [stepFunc1, stepFunc2, stepFunc3];
+
+// function* iteratorSteps(steps){
+// 	for (var i = 0; i < steps.length; i++){
+// 		var step = steps[i];
+// 		yield step();
+// 	}
+// };
+
+// // 任务 》步骤 使用 yield* 表达式遍历, 
+// // 最后使用 for of 循环一次性依次执行所有任务的所有步骤
+// let s = [job1, job2, job3];
+// function* iteratorJobs(jobs){
+// 	for (var i = 0; i < jobs.length; i++){
+// 		var job = jobs[i];
+// 		yield* iteratorSteps(job.steps);
+// 	}
+// }
+
+// for (var step of iteratorJobs(jobs)){
+// 	console.log(step.id);
+// }
 
 
 

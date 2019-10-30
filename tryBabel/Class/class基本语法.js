2029f057d27893593df8e9f6062160d9714096b8
2554509132@qq.com <2554509132@qq.com>
2019-10-30 17:00:35 +0800

    class 设计思想与 ES5 原型思想相同

--- tryBabel/Class/class基本语法.js
+++ tryBabel/Class/class基本语法.js
@@ -88,6 +88,71 @@ ES6 引入 Class 概念，作为对象的模板
 // console.log(Object.keys(Point.prototype));
 // console.log(Object.getOwnPropertyNames(Point.prototype));
 
+/*
+constructor 方法
+类的默认方法
+通过 new 命令生成对象实例时，自动调用
+一个类必须有 constructor 方法，
+没有显示定义，则默认添加空的 constructor
+*/
+
+// constructor 默认返回实例对象（即this）
+// class Foo {
+// 	constructor(){
+// 		return Object.create(null);// 返回的不是 Foo 实例
+// 	}
+// }
+// console.log(new Foo() instanceof Foo);
+
+
+/*
+类的实例
+要用 new 命令，不能函数式 调用
+*/
+
+
+/*
+实例属性与原型属性
+不是定义在本身，即 this，则就是定义在原型上的
+*/
+// example
+// class Point{
+// 	constructor(x, y){
+// 		this.x = x;
+// 		this.y = y;
+// 	}
+// 	toString(){
+// 		return '(' + this.x + ', ' + this.y + ')';
+// 	}
+// };
+
+// var point = new Point(2, 3);
+
+// console.log(point.toString());
+// console.log(point.hasOwnProperty('x'));
+// console.log(point.hasOwnProperty('y'));
+// console.log(point.hasOwnProperty('toString'));
+// console.log(point.__proto__.hasOwnProperty('toString'));
+
+/*
+为原型添加方法，使用 Objec.getPrototypeOf() 方法
+共享原型方法
+*/
+// var p1 = new Point(2, 3);
+// var p2 = new Point(3, 2);
+
+// p1.__proto__.printName = function(){
+// 	return 'Oops';
+// }
+// p1.printName()
+// p2.printName()
+
+// var p3 = new Point(4, 2);
+// p3.printName();
+
+
+
+
 
 
 
